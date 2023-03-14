e6fa639b048101351815691bb1da01542eb5ea10
fili0727 <filippa.arildsen@gmail.com>
2023-03-14 10:42:44 +0100

    Initial commit

---
+++ opgave2.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const tekst = "peter";
+
+const start = tekst.substring(0, 2);
+const slut = tekst.substring(3, 5);
+const bogstav = tekst.substring(2, 3);
+
+const stor = bogstav.toUpperCase();
+
+console.log(stor);
+console.log(start);
+console.log(slut);
+
+const complete = start + stor + slut;
+console.log(complete);
